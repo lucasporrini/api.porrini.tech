7d4e1df8a61b2b285adb43e654107829a7475b2a
lucasporrini <2608lucas@gmail.com>
2024-01-23 21:23:59 +0100

    🐞Debug: testing something

--- app/auto/autodeploy.sh
+++ app/auto/autodeploy.sh
@@ -2,11 +2,11 @@
 cd ../../
 
 # add the date UTC+1 to the log file
-date >> logs/auto/deploy.log
-date >> logs/auto/error.log
+date >> ./logs/auto/deploy.log
+date >> ./logs/auto/error.log
 
 # execute the command
-git pull origin main >> logs/auto/deploy.log
+git pull origin main >> ./logs/auto/deploy.log
 
 # move to current folder
 cd -
