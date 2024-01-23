37c8641d7244e52fa5864cb211a373dfacbddfea
lucasporrini <2608lucas@gmail.com>
2024-01-23 21:35:21 +0100

    🐞Debug: testing something

--- app/auto/autodeploy.sh
+++ app/auto/autodeploy.sh
@@ -3,10 +3,9 @@ cd ./
 
 # Ajouter la date dans les fichiers de log
 date >> logs/auto/deploy.log
-date >> logs/auto/error.log
 
 # Exécuter les commandes git et rediriger les sorties vers les fichiers de log
-git pull origin main >> logs/auto/deploy.log 2>> logs/auto/error.log
+git pull origin main >> logs/auto/deploy.log
 
 # Retour au répertoire initial si nécessaire
 cd -
