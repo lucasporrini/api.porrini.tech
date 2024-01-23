edceb35b788c6897201fc3bbc5f825290918bfcf
lucasporrini <2608lucas@gmail.com>
2024-01-23 19:45:40 +0100

    🛠️ Edited: add date to log file

--- app/auto/autodeploy.sh
+++ app/auto/autodeploy.sh
@@ -1,6 +1,10 @@
 # move to api folder
 cd ../../
 
+# add the date UTC+1 to the log file
+date >> logs/auto/deploy.log
+date >> logs/auto/error.log
+
 # execute the command
 git pull origin main >> logs/auto/deploy.log 2>> logs/auto/error.log
 
